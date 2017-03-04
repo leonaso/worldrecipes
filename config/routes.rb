2b908bfee8deeeaaf110b278089c63a6c0a057f8
lscloud <leonar_soto@yahoo.com>
2017-03-04 00:27:26 +0000

    Add show recipe

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   #resources :recipes
   get '/recipes', to: 'recipes#index'
+  get '/recipes/:id', to: 'recipes#show', as: 'recipe'
+  
 end
--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -3,9 +3,14 @@ require 'test_helper'
 class RecipesTest < ActionDispatch::IntegrationTest
   
   def setup
-    @chef1 = Chef.create!(chef_name: 'chef1', email: 'chef1@wrecipes.com')
-    @recipe = Recipe.create(recipe_name: "Chicken saute", description: "cook chiken", chef: @chef1)
-    @recipe2 = Recipe.create(recipe_name: "Vegies saute", description: "cook vegies", chef: @chef1)
+    @chef = Chef.create!(chef_name: 'chef', email: 'chef@wrecipes.com')
+    @recipe = Recipe.create(recipe_name: "Chicken saute", description: "cook chiken", chef: @chef)
+    @recipe2 = Recipe.create(recipe_name: "Vegies saute", description: "cook vegies", chef: @chef)
+  end
+
+  test "should get recipes index" do
+    get recipes_path
+    assert_response :success
   end
   
   test "should get recipes listing" do
@@ -13,10 +18,17 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_template 'recipes/index'
     assert_match @recipe.recipe_name, response.body
     assert_match @recipe2.recipe_name, response.body
+    assert_select "a[href=?]", recipe_path(@recipe),text: @recipe.recipe_name
+    assert_select "a[href=?]", recipe_path(@recipe2),text: @recipe2.recipe_name
+  end
+
+  test "should get recipes show" do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
+    assert_match @recipe.recipe_name, response.body
+    assert_match @recipe.description, response.body
+    assert_match @chef.chef_name, response.body
   end
   
-  test "should get recipes index" do
-    get recipes_path
-    assert_response :success
-  end
+    
 end
